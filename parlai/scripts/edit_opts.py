6079fdec1234a3307649b3a78da9551ba8a16177
Abi See <abigail.e.see@gmail.com>
2018-11-06 15:10:20 -0800

    opt scripts use json not pickle

--- parlai/scripts/edit_opts.py
+++ parlai/scripts/edit_opts.py
@@ -1,17 +1,22 @@
 import sys
 import pickle
+import json
 
 
 if __name__=="__main__":
     assert len(sys.argv)==2
     optfile = sys.argv[1]
-    with open(optfile, 'rb') as handle:
-        new_opt = pickle.load(handle)
+    # with open(optfile, 'rb') as handle:
+    #     new_opt = pickle.load(handle)
+    with open(optfile, 'r') as f:
+        new_opt = json.load(f)
 
     import pdb; pdb.set_trace()
 
     for key in sorted(new_opt.keys()):
         print(key, new_opt[key])
 
-    with open(optfile+'.new', 'wb') as handle:
-        pickle.dump(new_opt, handle)
+    # with open(optfile+'.new', 'wb') as handle:
+    #     pickle.dump(new_opt, handle)
+    with open(optfile+'.new', 'w') as f:
+        json.dump(new_opt, f)
--- parlai/scripts/inspect_opts.py
+++ parlai/scripts/inspect_opts.py
@@ -1,11 +1,14 @@
 import sys
 import pickle
+import json
 
 
 if __name__=="__main__":
     assert len(sys.argv)==2
     optfile = sys.argv[1]
-    with open(optfile, 'rb') as handle:
-        new_opt = pickle.load(handle)
+    # with open(optfile, 'rb') as handle:
+    #     new_opt = pickle.load(handle)
+    with open(optfile, 'r') as f:
+        new_opt = json.load(f)
     for key in sorted(new_opt.keys()):
         print(key, new_opt[key])
